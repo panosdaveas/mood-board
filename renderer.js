bacc225b694b4a87bd3768237b533d692da34fb0
panosdaveas <panosntaveas@gmail.com>
2025-02-08 19:54:00 +0200

    first commit

---
+++ renderer.js
@@ -0,0 +1,16 @@
+const setButton = document.getElementById("btn");
+const titleInput = document.getElementById("title");
+// setButton.addEventListener("click", () => {
+//   const title = titleInput.value;
+//   window.electronAPI.setTitle(title);
+// });
+
+const { ipcRenderer } = require("electron");
+
+document.getElementById("close-btn").addEventListener("click", () => {
+  ipcRenderer.send("close-app");
+});
+
+document.getElementById("minimize-btn").addEventListener("click", () => {
+  ipcRenderer.send("minimize-app");
+});
