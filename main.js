8d1acd8b978c3577dba3b83db8c445c89e3343ce
panosdaveas <panosdaveas@gmail.com>
2025-02-11 13:30:11 +0200

    IPC handlers

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 800,
-    // frame: false,
+    frame: false,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true,
